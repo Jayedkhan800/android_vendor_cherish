776decc461fba0efd40e126bc9860722de02c0d8
Jayed khan <tanvirgaming859@gmail.com>
2025-10-02 15:00:52 +0600

    Enable Blur

--- config/common.mk
+++ config/common.mk
@@ -297,3 +297,12 @@ TARGET_PRODUCT_PROP += vendor/cherish/config//gameprops/product.prop
 
 # Include extra packages
 include vendor/cherish/config/packages.mk
+
+#Blur
+ifeq ($(TARGET_ENABLE_BLUR), true)
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+endif
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=0
